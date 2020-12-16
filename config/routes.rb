19c483b59e754cce45065a56c850a50e6013ca66
ilia zolas <ilia@thedmcsa.co.za>
2020-12-16 17:36:00 +0100

    changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
-  # resources :cocktails
-
   root "cocktails#index"
   resources :cocktails, only: [:index, :show, :new, :create] do
   resources :doses, only: [:new, :create]
   end
-    resources :doses, only: [:destroy]
+  #   resources :doses, only: [:destroy]
 end
